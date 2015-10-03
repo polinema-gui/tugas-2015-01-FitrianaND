4f5b8c6661183e6e5667be5601d230482635526b
FitrianaND <f_dewi24@yahoo.co.id>
2015-10-03 05:57:27 -0700

    TUGAS implementasi mengubah warna form 1

---
+++ Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If MyBase.BackColor = Color.Red Then
+            MyBase.BackColor = Color.CadetBlue
+        Else
+            MyBase.BackColor = Color.Red
+        End If
+    End Sub
+End Class
